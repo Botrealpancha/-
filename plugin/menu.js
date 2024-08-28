7fed75cb2afb1a30ea072704352c50c370f9532e
Botrealpancha <shehanvimukthi200665@gmail.com>
2024-08-28 18:51:56 +0530

    Add files via upload

---
+++ plugin/menu.js
@@ -0,0 +1,70 @@
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get bot cmd.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const config = await readEnv();
+let menu = {
+ main: '',
+ download: '',
+ group: '',
+ owner: '',
+ convert: '',
+ search: '',
+ fun: '',
+};
+
+  for (let i = 0; i < commands.length; i++) { if (commands[i].pattern && !commands[i].dontAddCommandList) { menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`; } }
+
+ let madeMenu = `==🪐  ●●《Thenu-MD》●● ✨==
+
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤====================
+*《《Hello ${pushname} 》》*
+
+|● *This is Thenu-MD Wa Bot.*. ...👾
+<□□□□□□□□□□□□□□□□□□□□□=====================
+
+🥊● Download Command
+
+${menu.download}
+
+*🎟️■Main Commands*
+
+ ${menu.main}               
+
+*💎●Group Commands*
+
+${menu.group}
+
+*⚙️Owner Commands*
+
+${menu.owner}
+
+*🛡️Convert  Commands*
+
+${menu.convert}
+
+*💣Search Commands*
+
+${menu.search}
+
+*💕FUN COMMANDS*
+
+${menu.fun}
+
+> Powerd By Thenula_Panapiti...🔰 
+   `
+ 
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+
+  }catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
